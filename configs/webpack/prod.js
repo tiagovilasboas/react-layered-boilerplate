9b8da75f5a242c96827da2b53cf9c5e5e30d282a
Tiago Montanha <tcarvalhovb@gmail.com>
2025-07-03 18:25:02 -0300

    build(webpack): add css-minimizer and bundle analyzer plugins and register them in production config

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,6 +1,10 @@
 // production config
 const { merge } = require('webpack-merge');
 const { resolve } = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const commonConfig = require('./common');
 
@@ -8,10 +12,51 @@ module.exports = merge(commonConfig, {
   mode: 'production',
   entry: '@/pages/index.tsx',
   output: {
-    filename: 'js/bundle.[contenthash].min.js',
+    filename: 'js/[name].[contenthash].min.js',
+    chunkFilename: 'js/[name].[contenthash].chunk.js',
     path: resolve(__dirname, '../../dist'),
     publicPath: '/',
+    clean: true,
   },
   devtool: 'source-map',
-  plugins: [],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    // Bundle Analyzer - descomente para analisar o bundle
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'static',
+    //   openAnalyzer: false,
+    //   reportFilename: 'bundle-report.html',
+    // }),
+  ],
+  performance: {
+    hints: 'warning',
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 });
